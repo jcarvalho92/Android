f3e420d37ba558cf52d16efd8f9a50e2e977e48c
Juliana <julianadecarvalho92@gmail.com>
2020-10-18 22:39:41 -0400

    Initial Commit
    developed a Pizza app

---
+++ PizzaOnline/app/src/main/java/com/example/group5_mapd711_assign2_pizzaonline/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.group5_mapd711_assign2_pizzaonline
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //adding the Options menu in the activity
+        menuInflater.inflate(R.menu.main,menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val i = Intent(this, PizzaDetailsActivity::class.java)
+
+        //checking which menu was selected and passing it to the PizzaDetails Activity
+        when (item.itemId){
+            R.id.meatSupreme ->
+               i.putExtra("typeofPizza", "Meat Supreme")
+            R.id.superHawaiian ->
+                i.putExtra("typeofPizza", "Super Hawaiian")
+            R.id.veggie ->
+                i.putExtra("typeofPizza", "Veggie")
+            R.id.mediterranean ->
+                i.putExtra("typeofPizza", "Mediterranean")
+            R.id.cheddarSupreme ->
+                i.putExtra("typeofPizza", "Cheddar Supreme")
+        }
+
+        startActivity(i)
+        return true
+    }
+
+}
+
